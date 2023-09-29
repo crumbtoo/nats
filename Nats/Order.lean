789d1fb73cd4c70458de3f7841ad842bb17a7427
crumbtoo <crumb@disroot.org>
2023-09-29 12:29:18 -0600

    before the nuke

--- Nats/Order.lean
+++ Nats/Order.lean
@@ -2,6 +2,7 @@ import Nats.Basic
 import Nats.Addition
 import Nats.Logic
 import Mathlib.Tactic.Contrapose
+import Mathlib.Tactic.Use
 import Std.Tactic.Basic
 --------------------------------------------------------------------------------
 open Basic
@@ -46,6 +47,101 @@ theorem lt_expand (a b : nat) : a < b ↔ (∃ n, a + succ n = b) := by
     exact ha
   exact ⟨p, q⟩ 
 
+theorem zero_lt_succ (n : nat) : 0 < succ n := by
+  induction n with
+  | zero =>
+    rw [lt_expand]
+    use zero
+    simp
+  | succ d _ =>
+    rw [lt_expand]
+    use (succ d)
+    simp
+
+theorem succ_inj_lt (a b : nat) : succ a < succ b ↔ a < b := by
+  have p : succ a < succ b -> a < b := by
+    intro ha
+    induction b with
+    | zero =>
+      apply Exists.elim ha $ λ w => by
+        intro hb
+        exfalso
+        rw [add_succ, succ_inj_iff, succ_add] at hb
+        exact succ_ne_zero _ hb
+    | succ d _ =>
+      apply Exists.elim ha $ λ w => by
+        intro ha'
+        rw [add_succ, succ_inj_iff, succ_add, succ_inj_iff] at ha'
+        use w
+        rw [add_succ, succ_inj_iff]
+        exact ha'
+  have q : a < b -> succ a < succ b := by
+    intro ha
+    induction b with
+    | zero =>
+      apply Exists.elim ha $ λ w => by
+        intro ha'
+        exfalso
+        rw [add_succ] at ha'
+        exact succ_ne_zero _ ha'
+    | succ d _ =>
+      apply Exists.elim ha $ λ w => by
+        intro ha'
+        rw [add_succ, succ_inj_iff] at ha'
+        use w
+        rw [add_succ, succ_inj_iff, succ_add, succ_inj_iff]
+        exact ha'
+  exact ⟨p, q⟩ 
+
+theorem ne_is_lt_or_gt (a b : nat) : a ≠ b ↔ (a < b ∨ a > b) := by
+  have p : a ≠ b -> (a < b ∨ a > b) := by
+    intro ha
+    /- rw [GT.gt, lt_expand, lt_expand] -/
+    induction a generalizing b with
+    | zero =>
+      cases b with
+      | zero =>
+        contradiction
+      | succ d =>
+        left
+        exact zero_lt_succ _
+    | succ d ih =>
+      cases b with
+      | zero =>
+        right
+        rw [GT.gt]
+        exact zero_lt_succ _
+      | succ e =>
+        have hb : d ≠ e := by
+          intro hb
+          rw [<- succ_inj_iff] at hb
+          contradiction
+        have hc : d < e := by
+          ih e hb
+        left
+  have q : (a < b ∨ a > b) -> a ≠ b := by
+    sorry
+  exact ⟨p, q⟩ 
+
+theorem lt_add (a b n : nat) : a < b -> a < b + n := by
+  intro h
+  induction a with
+  | zero =>
+    cases n with
+    | zero =>
+      rw [zero_is_0, add_zero]
+      exact h
+    | succ d =>
+      rw [add_succ]
+      exact zero_lt_succ _
+  | succ d ih =>
+    cases n with
+    | zero =>
+      rw [zero_is_0, add_zero]
+      exact h
+    | succ e =>
+      rw [add_succ]
+
 theorem ge_iff_not_lt (a b : nat) : a ≥ b ↔ ¬ a < b := by
   have p : a ≥ b -> ¬ a < b := by
     intro ha hb
@@ -59,8 +155,15 @@ theorem ge_iff_not_lt (a b : nat) : a ≥ b ↔ ¬ a < b := by
         exact n_add_succ_ne_n _ hb'
 
   have q : ¬ a < b -> a ≥ b := by
+    contrapose
+    rw [not_not]
     intro ha
-    sorry
+    induction a with
+    | zero =>
+      sorry
+    | succ d ih =>
+      sorry
+    
   exact ⟨p, q⟩ 
 
 theorem gt_expand (a b : nat) : a > b ↔ ¬ (∃ n, a + n = b) := by
