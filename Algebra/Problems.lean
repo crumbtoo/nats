89945d38b52cbfb84f9fc4ec6d30bb7577604cdb
crumbtoo <crumb@duck.com>
2023-10-06 17:57:00 -0600

    idk lol

--- Algebra/Problems.lean
+++ Algebra/Problems.lean
@@ -7,27 +7,32 @@ import Algebra.Lemmas.Group
 namespace Problems
 --------------------------------------------------------------------------------
 
-def Abelian (T : Type*) [Group T] := ∀ (a b : T), a*b = b*a
-def AbelianAdd (T : Type*) [AddGroup T] := ∀ (a b : T), a+b = b+a
-
 lemma elem_or_eq {n : ℕ}
         : n ∈ ({100, 101, 102} : Finset ℕ) ↔ n = 100 ∨ n = 101 ∨ n = 102
         := by
   sorry
 
+def Commutative (G : Type) [Group G] := ∀ (a b : G), a*b = b*a
+
+lemma commutative_group_is_CommGroup [Group G]
+      (h : Commutative G)
+      : CommGroup G
+      :=
+  { mul_comm := h }
+
 -- Suppose G is a group where (ab)ⁿ = aⁿbⁿ for all a, b ∈ G and n ∈ {100, 101,
 -- 102}. Prove that G is abelian.
 example [Group G]
         : ∀ (a b : G) (n : ({100, 101, 102} : Finset ℕ)),
-        (a*b)^↑↑n = a^↑↑n * b^↑↑n
-        -> CommGroup G
+          (a*b)^↑↑n = a^↑↑n * b^↑↑n
+          -> Commutative G
         := by
-  intro a b n h
+  intro a b n
   have i : n.val = 100 ∨ n.val = 101 ∨ n.val = 102 := by
     have j := n.property
     rw [elem_or_eq] at j
     exact j
-
-  rw [elem_or_eq] at n
-
+  contrapose
+  intro h
+  sorry
 
